b41d5516ed3897eed2abb0308e400dcfb767b735
Bede Carroll <4151340+bedecarroll@users.noreply.github.com>
2025-06-11 16:15:33 -0700

    feat(nix): add flake for macfmt

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "macfmt - format MAC addresses";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in {
+        packages.default = pkgs.rustPlatform.buildRustPackage {
+          pname = "macfmt";
+          version = "0.1.0";
+          src = self;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        apps.default = flake-utils.lib.mkApp {
+          drv = self.packages.${system}.default;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.rustc
+            pkgs.cargo
+            pkgs.clippy
+            pkgs.rustfmt
+          ];
+        };
+      });
+}
